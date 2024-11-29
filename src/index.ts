f84385b569bf45b11064de407b9b47a643973a79
Abhilash Mandula <mandula.abhilash@gmail.com>
2024-11-29 09:52:15 +0530

    Update the project to javascript and remove typescript support

--- src/index.ts
+++
@@ -1,3 +0,0 @@
-export { createAxiosInstance } from './axios-instance';
-export { tokenManager, setCustomTokenManager } from './token-manager';
-export type { ApiConfig, ApiResponse, TokenManager } from './types';
