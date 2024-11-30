f4fb0e9701e4f7d8d7e2d68e2233ad3ab865cf92
Abhilash Mandula <mandula.abhilash@gmail.com>
2024-11-30 12:26:56 +0530

    Remove the use of client side token manager

--- src/axios-instance.js
+++ src/axios-instance.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 import axiosRetry from "axios-retry";
-import { tokenManager } from "./token-manager.js";
 
 export const createAxiosInstance = (config) => {
   const instance = axios.create({
@@ -12,9 +11,8 @@ export const createAxiosInstance = (config) => {
 
   const defaultOnUnauthorized = () => {
     console.log("User is unauthorized. Logging out...");
-    tokenManager.removeTokens();
     if (typeof window !== "undefined") {
-      window.location.href = "/";
+      window.location.href = "/login";
     }
   };
 
@@ -39,92 +37,19 @@ export const createAxiosInstance = (config) => {
     },
   });
 
-  let isRefreshing = false;
-  let failedQueue = [];
-
-  const processQueue = (error, token = null) => {
-    failedQueue.forEach((prom) => {
-      if (error) {
-        prom.reject(error);
-      } else {
-        prom.resolve(token);
-      }
-    });
-    failedQueue = [];
-  };
-
-  // Request interceptor
-  instance.interceptors.request.use(
-    (config) => {
-      const token = tokenManager.getAccessToken();
-      if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-      }
-      return config;
-    },
-    (error) => Promise.reject(error)
-  );
-
   // Response interceptor
   instance.interceptors.response.use(
-    (response) => {
-      // Extract tokens from response headers if present
-      const accessToken = response.headers["x-access-token"];
-      const refreshToken = response.headers["x-refresh-token"];
-
-      if (accessToken && refreshToken) {
-        tokenManager.setTokens(accessToken, refreshToken);
-      }
-
-      return response;
-    },
+    (response) => response,
     async (error) => {
-      const originalRequest = error.config;
-
-      if (error.response?.status === 401 && !originalRequest._retry) {
-        if (isRefreshing) {
-          return new Promise((resolve, reject) => {
-            failedQueue.push({ resolve, reject });
-          })
-            .then((token) => {
-              originalRequest.headers.Authorization = `Bearer ${token}`;
-              return instance(originalRequest);
-            })
-            .catch((err) => Promise.reject(err));
-        }
-
-        originalRequest._retry = true;
-        isRefreshing = true;
-
-        try {
-          const refreshToken = tokenManager.getRefreshToken();
-          if (!refreshToken) throw new Error("No refresh token available");
-
-          const response = await instance.post("/api/auth/refresh-token", {
-            refreshToken,
-          });
-
-          const { accessToken: newAccessToken, refreshToken: newRefreshToken } =
-            response.data;
-          tokenManager.setTokens(newAccessToken, newRefreshToken);
-
-          instance.defaults.headers.common.Authorization = `Bearer ${newAccessToken}`;
-          originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
-
-          processQueue(null, newAccessToken);
-          return instance(originalRequest);
-        } catch (refreshError) {
-          processQueue(refreshError, null);
-          tokenManager.removeTokens();
-          (config.onUnauthorized || defaultOnUnauthorized)();
-          return Promise.reject(refreshError);
-        } finally {
-          isRefreshing = false;
-        }
+      if (error.response?.status === 401) {
+        // Log out the user by redirecting to login page
+        (config.onUnauthorized || defaultOnUnauthorized)();
+        return Promise.reject(error);
       }
 
       if (error.response?.status === 403) {
         (config.onForbidden || defaultOnForbidden)();
+        return Promise.reject(error);
       }
 
       return Promise.reject(error);
